311e18641d42354d8c010ebbfb626b4334889797
julasweta <julasweta@ukr.net>
2024-09-30 19:33:11 +0300

    start

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as passport from 'passport';
+import * as cors from 'cors';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cors());
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  // Налаштування Passport
+  app.use(passport.initialize());
+
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Vignettes')
+    .setDescription('Description Project')
+    .setVersion('1.0.')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT, () => {
+    Logger.log(`http://${process.env.HOST}:${process.env.PORT}/api`, 'Doc');
+  });
+}
+bootstrap();
